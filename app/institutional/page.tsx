a23ebe3e6f8e51793b9b3ee132262186c77b3afa
PDthePlug <denver.mpofu09@gmail.com>
2026-08-30 07:23:57 +0200

    add institutional console route

---
+++ app/institutional/page.tsx
@@ -0,0 +1,6 @@
+import "./institutional.css";
+import { InstitutionalConsole } from "@/components/bis/InstitutionalConsole";
+
+export default function InstitutionalPage() {
+  return <InstitutionalConsole />;
+}
