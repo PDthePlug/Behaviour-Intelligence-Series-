f31d28b5876f36895f111bd6ae159df19344a85d
PDthePlug <denver.mpofu09@gmail.com>
2026-08-30 07:42:42 +0200

    Add public learner invitation route

---
+++ app/join/page.tsx
@@ -0,0 +1,6 @@
+import "../institutional/self-service.css";
+import { JoinExperience } from "@/components/bis/JoinExperience";
+
+export default function JoinPage() {
+  return <JoinExperience />;
+}
